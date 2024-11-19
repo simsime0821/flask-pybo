a0f7b7fdb3cbe7db4fe6413b3a5b763be754f3ae
최시환 <zooozooo1@naver.com>
2024-11-19 21:21:02 +0900

    ,     static_folder="/home/master/projects/myproject/pybo/static",  # 실제 경로     static_url_path="/static" 삭제

--- config/production.py
+++ config/production.py
@@ -1,5 +1,30 @@
 from config.default import *
+from logging.config import dictConfig
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b')\xf0[\xd8F^)!y\xdek\x7f\xf0\xa7]\x8a'
+
+
+dictConfig({
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        }
+    },
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/myproject.log'),
+            'maxBytes': 1024 * 1024 * 5,  # 5 MB
+            'backupCount': 5,
+            'formatter': 'default',
+        },
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['file']
+    }
+})
